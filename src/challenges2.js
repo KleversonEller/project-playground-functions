020bbafe7919a1cc72d63251c944f9cf96b35478
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-02 23:28:45 -0300

    Desafio 11 'generatePhoneNumber'

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,8 +4,28 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+let resultado = null;
+let cont = 0;
+
+resultado = '('+array[0]+array[1]+')'+' '+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];  
+  if (array.length !== 11){
+    resultado = "Array com tamanho incorreto.";
+  }else { 
+    for(let possicao of array){
+      if (cont >= 3 || possicao < 0 || possicao > 9){
+        resultado = "não é possível gerar um número de telefone com esses valores";
+      }
+      cont = 0;
+      for(let possicao2 of array){
+         if (possicao === possicao2){
+           cont += 1;
+         }
+      }
+    }
+  }
+ return resultado;
 }
 
 // Desafio 12
